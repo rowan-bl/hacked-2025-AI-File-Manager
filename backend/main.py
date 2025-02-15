b7c3ce7208afa75feda841f9bf3d1a0c8ca5ecb6
Phillip <phillipcorporon@hotmail.com>
2025-02-15 15:08:44 -0700

    Got a return from deepseek while running ollama

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,31 @@
 import os
+import sys
 from dotenv import load_dotenv
+import requests
+import json
+
+load_dotenv()
+
+OLLAMA_URL = os.getenv("OLLAMA_URL")
+
+print(OLLAMA_URL)
+
+headers = {
+    "content-type": "application/json"
+}
+
+data = {
+    "model": "deepseek-r1:8b",
+    "prompt": "Why is the sky blue",
+    "stream": False
+}
+response = requests.post(OLLAMA_URL,headers=headers, data=json.dumps(data))
+
+if response.status_code == 200:
+    print('response code = 200')
+    response_text = response.text
+    data = json.loads(response_text)
+    actual_response = data["response"]
+    print(actual_response)
+else:
+    print("error", response.status_code)

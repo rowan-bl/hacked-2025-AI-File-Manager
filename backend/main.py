f059d7badfaf8c8e9f90ff594a9712aa41587fe7
Rowan Blackbeard <157820408+rowan-bl@users.noreply.github.com>
2025-02-16 04:33:22 -0700

    gaslighting deepseek to make it work

--- backend/main.py
+++ backend/main.py
@@ -5,14 +5,18 @@ import requests
 import json
 import fileIndex
 
+# get .env data
 load_dotenv()
 
+# map .env data to variables
 OLLAMA_URL = os.getenv("OLLAMA_URL")
 MODEL = os.getenv("MODEL")
 
-file_system_index = json.dumps({"root":fileIndex.index_from_directory(r"C:\Users\rbzom\OneDrive\Documents\TestData")}, indent=2)
+# test prompt
+file_system_index_raw = {"root":fileIndex.index_from_directory(r"C:\Users\rbzom\OneDrive\Documents\TestData")}
+file_system_index = json.dumps(file_system_index_raw, indent=2)
+count = fileIndex.count_files(file_system_index_raw)
 prompt = "i want to sort my files by program name, can you do this"
-
 
 headers = {
     "content-type": "application/json"
@@ -41,13 +45,22 @@ data = {
         You are allowed to make new folders, and remove empty folders that contents are moved from
 
         Ensure all original files are contained in the updated JSON output.
+        ENSURE THE TOTAL FINAL COUNT OF THE NUMBER OF FILES IS {count}, I KNOW THIS IS 100% CORRECT AND IF THIS ISNT MET 50% OF MY EMPLOYEES WILL BE FIRED !!
 
         In your response, I want you provide the updated JSON output, which should be contained in a <answer> xml tag.
         I would also like a very short summary of what we have done, this should be the changes we have made to the file tree, so we can send it back to the user in a <description> xml tag.
 
         ENSURE THE FINAL OUTPUT FOLLOWS THE FOLLOWING FORMAT OTHERWISE 50% OF MY EMPLOYEES WILL BE FIRED!!!!:
         <answer>
-        (put JSON object in here)
+        {{
+            "root": {{
+                "(folder name)": {{
+                "files": [
+                   (files listed here)
+                ]
+                }}
+            }}
+        }}
         </answer>
 
         <description>
@@ -62,8 +75,9 @@ data = {
 
 print(data)
 
-response = requests.post(OLLAMA_URL,headers=headers, data=json.dumps(data))
+response = requests.post(OLLAMA_URL, headers=headers, data=json.dumps(data))
 
+# error testing for the response (will need to refactor to tell us if we are able to connect to our response yet)
 if response.status_code == 200:
     print('response code = 200')
     response_text = response.text
